8ef63239125821fe918f950f3c28bc4ed83a9399
serkanyilmaz8 <serkanyilmaz8@posta.mu.edu.tr>
2022-09-15 12:37:48 +0300

    main olusturuldu

---
+++ eclipseGithub/src/main/java/eclipseGithub/Main.java
@@ -0,0 +1,11 @@
+package eclipseGithub;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("main olusturuldu");
+
+	}
+
+}
